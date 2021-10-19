078e5634907b13fb59bdf706122d4b738e121618
Funghu-fps <omer.cevruk@outlook.com>
2021-10-19 19:32:16 +0300

    First commit of CUMa branch
    deleted menu and menus class
    changed single class's name to MenuElement  changed some variable names

--- SPEEDmenu.py
+++ SPEEDmenu.py
@@ -1,130 +1,64 @@
+from typing import Text
 import pygame
-#use RGB only! 
-#text alling_x has left ,right and middle as options
-#text alling_y has up, down and middle
-#middle is default for both
+from pygame import draw
+from pygame.draw import rect
+from pygame.rect import Rect
+# use RGB only!
+# text alling_x has left ,right and middle as options
+# text alling_y has up, down and middle
+# middle is default for both
 
-#after crating a "single" object u can use single.render() to render it on the screen
-class single:       #screen is pygame.surface class                                                                               with,height                            
-    def __init__(self, screen, pos: tuple, font, color: tuple, msg: str, background: bool = False, backcolor: tuple = (0, 0, 0), size: tuple = (30, 30), width: int = 0, textAling_x: str = "middle", textAling_y: str = "middle"):
-        # variables            position is (x,y)
-        self.screen = screen
-        self.pos = pos
-        self.font = font
-        self.color = color
-        self.msg = msg
-        self.background = background
-        self.backcolor = backcolor
-        self.size = size
-        self.width = width
-        self.textAling_x = textAling_x
-        self.textAling_y = textAling_y
 
-        self.text = 0
-        self.text_x = 0
-        self.text_y = 0
+class MenuElement:
+    """
+    MenuElement class creates Menu elements U can only use RGB values 
+    pos(x,y) represents the left top corner of the rectangle
+    """
+    # full constructor
 
-        # Middle
-        if self.textAling_x == "middle":
-            self.text = self.font.render(self.msg, True, self.color)
-            self.text_x = self.size[0]/2 - \
-                self.text.get_width()/2 + self.pos[0]
-        if self.textAling_y == "middle":
-            self.text = self.font.render(self.msg, True, self.color)
-            self.text_y = self.size[1]/2 - \
-                self.text.get_height()/2 + self.pos[1]
-
-        # none
-        if self.textAling_x == "none" or self.textAling_x == "left":
-            self.text = self.font.render(self.msg, True, self.color)
-            self.text_x = self.pos[0]
-        if self.textAling_y == "none" or self.textAling_y == "up":
-            self.text = self.font.render(self.msg, True, self.color)
-            self.text_y = self.pos[1]
-
-        if self.textAling_x == "right":
-            self.text = self.font.render(self.msg, True, self.color)
-            self.text_x = (self.size[0]-self.text.get_width())+self.pos[0]
-        if self.textAling_y == "down":
-            self.text = self.font.render(self.msg, True, self.color)
-            self.text_y = (self.size[1]-self.text.get_height())+self.pos[1]
-
-    # render
-    #rendering both text and box
-    def render(self):
-        if self.background:
-            pygame.draw.rect(self.screen, self.backcolor,
-                             (self.pos, self.size), self.width)
-            self.screen.blit(self.text, (self.text_x, self.text_y))
-            
-
-        # just text
-        else:
-            self.screen.blit(self.text, (self.text_x, self.text_y))
-            
-
-#menu class 
-
-#menu using singles creates a menu and singles are stored in a list and u can change or edit them when ever u want
-#use menu.render() to render
-class menu:             #screen is pygame.surface class
-    def __init__(self, screen, pos: tuple, font, color: tuple, collumNline: tuple, gap: int, *msg: str, background=False, backcolor: tuple = (0, 0, 0), size=(30, 30), width=0, backgroundcolor=(255, 0, 0), textAling_x="middle", textAling_y="middle"):
-        # variables
-        self.menu_elements = []#singles here :)
-        self.menu_rects = []#pygame rect objects
-        self.gapx,  self.gapy = gap
-        self.posx,  self.posy = pos
-        self.sizex,  self.sizey = size
-        self.collum,  self.line = collumNline
-        self.a = 0
+    def __init__(self,
+                 screen: pygame.Surface,
+                 pos: tuple[float, float],
+                 size: tuple[float, float],
+                 font: pygame.font.Font,
+                 text: str,
+                 text_alingment: tuple[str, str] = ("middle", "middle"),
+                 rect_color: tuple[int, int, int] = (0, 0, 0),
+                 text_color: tuple[int, int, int] = (255, 255, 255),
+                 text_anti_alias: bool = True,
+                 draw_rect: bool = True,
+                 border_radius: int = 0) -> None:
 
         self.screen = screen
         self.pos = pos
-        self.font = font
-        self.color = color
-        self.msg = msg
-        self.background = background
-        self.backcolor = backcolor
         self.size = size
-        self.width = width
-        self.backgroundcolor = backgroundcolor
-        self.textAling_x = textAling_x
-        self.textAling_y = textAling_y
-        for x in range(self.line):
-            for i in range(self.collum):
-                menu_element = single(self.screen, (self.posx, self.posy),  self.font,  self.color,self.msg[i + self.a],  self.background,  self.backcolor,  self.size,  self.width, self.textAling_x, self.textAling_y)
-                self.menu_elements.append(menu_element)
-                self.menu_rects.append(pygame.Rect(
-                    (self.posx, self.posy), self.size))
+        self.font = font
+        self.text = text
+        self.text_alingment = text_alingment
+        self.rect_color = rect_color
+        self.text_color = text_color
+        self.text_anti_alias = text_anti_alias
+        self.draw_rect = draw_rect
+        self.border_radius = border_radius
 
-                self.posx = self.posx + self.gapx + self.sizex
-            if self.collum != 1:
-                self.a += 2
-            else:
-                self.a += 1
-            self.posx = self.pos[0]
-            self.posy = self.posy + self.sizey + self.gapy
+        self.rect = Rect(pos, size)
+        self.text_pos_x, self.text_pos_y = 0, 0
+        self.text_size: tuple[float, float] = self.font.size(self.text)
+        self.rendered = None
+
+        if self.text_alingment[0] == "middle":
+            self.rendered = self.font.render(
+                self.text, self.text_anti_alias, self.text_color, False)
+            self.text_pos_x = self.size[0]/2 - \
+                self.text_size[0]/2 + self.pos[0]
+
+        if self.text_alingment[1] == "middle":
+            self.rendered = self.font.render(
+                self.text, self.text_anti_alias, self.text_color)
+            self.text_pos_y = self.size[1]/2 - \
+                self.text_size[1]/2 + self.pos[1]
 
     def render(self):
-
-        for i in self.menu_elements:
-            i.render()
-        pass
-
-# menus class can be used to manage multiple menu
-#menus are kept in a dict so ucan edit them as well
-#create menus object then use addmenu(keyword,menuobject) method to creare and add menus to menus.menus dict
-#you can render menus by menus.render(key)
-class menus(object):
-    def __init__(self):
-        self.menus = {
-
-        }
-        self.activeMenu = []
-
-    def addMenu(self, name: str, menu: menu):
-        self.menus[name] = menu
-        pass
-
-    def render(self, key: str):
-        self.menus[key].render()
+        pygame.draw.rect(self.screen, self.rect_color,
+                         self.rect, border_radius=self.border_radius)
+        self.screen.blit(self.rendered, (self.text_pos_x, self.text_pos_y))
--- example.py
+++ example.py
@@ -1,39 +1,27 @@
-#classic pygame stuff
 import pygame
-from menu import * #import
-(width, height) = (700, 700)#window size
+
+from SPEEDmenu import MenuElement
+#from SPEEDmenu import * 
+
+(width, height) = (700, 700)
 running = True
 pygame.init()
 
 #setting display
 screen = pygame.display.set_mode((width, height))
+print(type(screen))
 pygame.display.set_caption("GAME")
 
 
-font1 = pygame.font.SysFont("Arial",25)#pygame font object must be created to use in single/menu objects
+font1 = pygame.font.SysFont("Arial",25)
+print(type(font1))
 
-
-
-
-
-#creating menus object 
-menus = menus()
-
-#adding menu to menus object. screen is pygame.surface
-#menus.addMenu("keyword", menu object)
-menus.addMenu("Test",  menu(screen, (0,0), font1, (255,255,255), (2,3), (10,10), "elma yes","elma yes","elma no","elma","elma","elma", background = True,  backcolor = (255,0,0), size = (100,50), width = 0, backgroundcolor = (0,255,0),   textAling_x = "middle",textAling_y = "middle"))
-#another one
-menus.addMenu("menu", menu(screen, (300,0), font1, (255,255,255), (1,3), (10,10), "continue", "New Game", "Test", background = True, backcolor = (0,255,0),  size = (100,50), width = 0, backgroundcolor = (255,0,0),  textAling_x = "middle",textAling_y = "middle"))
-#setting active menu                    
-menus.activeMenu = menus.menus["menu"]
-
+element = MenuElement(screen,(width/2, height/2),(80,60),font1,"SKRRRT", rect_color = (0,255,0))
 
 def main():
-    global running, screen, colors, showmenu, ev
+    running = True
     screen.fill((255,0,0))
     clock = pygame.time.Clock()
-    showmenu = True
-
     while running:
         clock.tick(60)
         ev = pygame.event.get()
@@ -44,45 +32,18 @@ def main():
                 #1== leftclick 2== middle click 3== right click 4== scroll
                 #forward 5== scroll back
                 if event.button == 1:
-                    checkhitmenu()
+                    pass
 
             if event.type == pygame.QUIT:
                running = False
                break
 
-        if showmenu == True:
-            #filling  screen
-            screen.fill(menus.activeMenu.backgroundcolor)
-            showmenu = False
-            menus.activeMenu.render()#rendering menu
-
-        pygame.display.flip()#bu satır tüm ekranı yeniliyor
-#clicking on menus
+        pygame.display.flip()
+        element.render()
 def get_MousePos():
     pos = pygame.mouse.get_pos()
     return (pos)
-def checkhitmenu():
-    global  showmenu, ev
-    posx,posy = get_MousePos()
-    a = 0
-    #if clicked on menu element
-    for i in menus.activeMenu.menu_rects:
-        if posx >= i.left and posx <= i.right and posy >= i.top and posy <= i.bottom :
-            print(menus.activeMenu.menu_elements[a].msg)
-            msg = menus.activeMenu.menu_elements[a].msg
-#___________game______________________________________________________________________________________________________________________________________________________________________________________
-            try:        
-                #if menus contains a menu named "msg"  the "msg" menu will become active menu
-                #similar things can be used to start events
-                    skrrt = menus.menus.get(msg)
-                    showmenu = True
-                    if skrrt:
-                        menus.activeMenu = skrrt
 
 
-            except :
-                pass
-        a+=1
-        pass
 if __name__ == '__main__':
    main()
